6f98367394c6798117858ba91d1c5f1e2e95ca8c
2643133462@qq.com <2643133462@qq.com>
2019-05-28 11:07:35 +0800

    springboot整合redis

---
+++ src/test/java/com/qf/springboot_redis/SpringbootRedisApplicationTests.java
@@ -0,0 +1,27 @@
+package com.qf.springboot_redis;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRedisApplicationTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void contextLoads() {
+
+        redisTemplate.opsForValue().set("name","milian");
+
+        Object name = redisTemplate.opsForValue().get("name");
+
+        System.out.println(name);
+    }
+
+}
